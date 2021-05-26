08e7be178740a99265fafdb097483fe175f0dd1b
ZubkovAC <3y6kob90@gmail.com>
2021-05-26 16:36:37 +0300

    Updates

---
+++ precache-manifest.66297625e86242d2e97edc56465192c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7440d79b42e7b322e3e80e9770c9653",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "0c62ab2cd8eea52edcd3",
+    "url": "/TodoList/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "032145f1104dcd3cf14c",
+    "url": "/TodoList/static/js/2.836167c6.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/TodoList/static/js/2.836167c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c62ab2cd8eea52edcd3",
+    "url": "/TodoList/static/js/main.9a1134a8.chunk.js"
+  },
+  {
+    "revision": "6f4002f704c746794b97",
+    "url": "/TodoList/static/js/runtime-main.b918c17e.js"
+  }
+]);
